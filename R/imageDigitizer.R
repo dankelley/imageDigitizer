8080025be7ffde62a87638d54f88c9df5999bc10
dankelley <kelley.dan@gmail.com>
2020-09-29 15:12:34 -0300

    use roxygen2 to create the NAMESPACE

--- R/imageDigitizer.R
+++ R/imageDigitizer.R
@@ -29,25 +29,25 @@ msg <- function(...)
     cat(file=stderr(), ...)
 }
 
-ui <- fluidPage(tags$script('$(document).on("keypress", function (e) { Shiny.onInputChange("keypress", e.which); Shiny.onInputChange("keypressTrigger", Math.random()); });'),
-                uiOutput(outputId="title"),
+ui <- shiny::fluidPage(tags$script('$(document).on("keypress", function (e) { Shiny.onInputChange("keypress", e.which); Shiny.onInputChange("keypressTrigger", Math.random()); });'),
+                shiny::uiOutput(outputId="title"),
                 #h5(paste("imager", version)),
                 #fluidRow(column(1, checkboxInput("debug", h6("Debug"), value=!FALSE))),
-                fluidRow(conditionalPanel(condition="output.stage == 1",
-                                          column(3, uiOutput(outputId="readImage")),
-                                          column(3, sliderInput("rotate", h5("Rotate [deg]"),
+                shiny::fluidRow(conditionalPanel(condition="output.stage == 1",
+                                          column(3, shiny::uiOutput(outputId="readImage")),
+                                          column(3, shiny::sliderInput("rotate", shiny::h5("Rotate [deg]"),
                                                                 min=-10, max=10, value=0, step=0.2)),
-                                          column(3, radioButtons("grid", label=h5("Grid"),
+                                          column(3, shiny::radioButtons("grid", label=shiny::h5("Grid"),
                                                                  choices=c("None"="off", "Fine"="fine", "Medium"="medium", "Coarse"="coarse"),
                                                                  selected="medium", inline=TRUE)),
-                                          column(2, actionButton("stage1button", "Proceed to next stage")))),
+                                          column(2, shiny::actionButton("stage1button", "Proceed to next stage")))),
                 fluidRow(conditionalPanel(condition="output.stage == 3",
-                                          column(3, textInput("xname", h5("Name horiz. axis"))),
-                                          column(3, textInput("yname", h5("Name vert. axis"))))),
-                fluidRow(column(2, actionButton("undo", "Undo")),
-                         column(2, actionButton("save", "Save results"))),
-                fluidRow(column(2, htmlOutput("status")),
-                         column(10, plotOutput("plot", click="plotClick", hover="plotHover", height=600)))
+                                          column(3, shiny::textInput("xname", shiny::h5("Name horiz. axis"))),
+                                          column(3, shiny::textInput("yname", shiny::h5("Name vert. axis"))))),
+                fluidRow(column(2, shiny::actionButton("undo", "Undo")),
+                         column(2, shiny::actionButton("save", "Save results"))),
+                fluidRow(column(2, shiny::htmlOutput("status")),
+                         column(10, shiny::plotOutput("plot", click="plotClick", hover="plotHover", height=600)))
                 )
 
 server <- function(input, output)
@@ -65,7 +65,7 @@ server <- function(input, output)
                           xaxisModel=NULL, yaxisModel=NULL,
                           xhover=NULL, yhover=NULL)
 
-  observeEvent(input$stage1button,
+  shiny::observeEvent(input$stage1button,
                {
                  state$stage <- 2
                }
@@ -78,7 +78,9 @@ server <- function(input, output)
 #               }
 #  )
 
-  observeEvent(input$keypressTrigger,
+  #'
+  #' @importFrom shiny HTML modalDialog showModal
+  shiny::observeEvent(input$keypressTrigger,
                {
                  if (state$step == 6) {
                    key <- intToUtf8(input$keypress)
@@ -101,23 +103,23 @@ server <- function(input, output)
 
   xAxisModal <- function(failed=FALSE)
   {
-    modalDialog(textInput("xAxisValue", "Enter x at last mouse click"),
-                footer=tagList(modalButton("Cancel"), actionButton("xAxisButtonOk", "OK")))
+    modalDialog(shiny::textInput("xAxisValue", "Enter x at last mouse click"),
+                footer=shiny::tagList(shiny::modalButton("Cancel"), shiny::actionButton("xAxisButtonOk", "OK")))
   }
   xAxisNameModal <- function(failed=FALSE)
   {
-    modalDialog(textInput("xAxisName", "Enter name of x axis"),
-                footer=tagList(modalButton("Cancel"), actionButton("xAxisNameButtonOk", "OK")))
+    modalDialog(shiny::textInput("xAxisName", "Enter name of x axis"),
+                footer=shiny::tagList(shiny::modalButton("Cancel"), shiny::actionButton("xAxisNameButtonOk", "OK")))
   }
   yAxisModal <- function(failed=FALSE)
   {
-    shiny::modalDialog(textInput("yAxisValue", "Enter y at last mouse click"),
-                footer=tagList(modalButton("Cancel"), actionButton("yAxisButtonOk", "OK")))
+    shiny::modalDialog(shiny::textInput("yAxisValue", "Enter y at last mouse click"),
+                footer=shiny::tagList(shiny::modalButton("Cancel"), shiny::actionButton("yAxisButtonOk", "OK")))
   }
   yAxisNameModal <- function(failed=FALSE)
   {
-    shiny::modalDialog(textInput("yAxisName", "Enter name of y axis"),
-                footer=tagList(shiny::modalButton("Cancel"), actionButton("yAxisNameButtonOk", "OK")))
+    shiny::modalDialog(shiny::textInput("yAxisName", "Enter name of y axis"),
+                footer=shiny::tagList(shiny::modalButton("Cancel"), shiny::actionButton("yAxisNameButtonOk", "OK")))
   }
 
   output$title <- shiny::renderUI({
@@ -125,10 +127,11 @@ server <- function(input, output)
   })
 
   output$readImage <- shiny::renderUI({
-    fileInput("inputFile", shiny::h5("Input file"), accept=c("image/png"))
+    shiny::fileInput("inputFile", shiny::h5("Input file"), accept=c("image/png"))
   })
 
-  output$plot <- renderPlot({
+  #' @importFrom graphics abline box par points rasterImage text
+  output$plot <- shiny::renderPlot({
     par(mar=rep(1, 4))
     asp <- if (is.null(state$image)) 1 else dim(state$image)[1] / dim(state$image)[2]
     plot(0:1, 0:1, type='n', asp=asp, xaxs="i", yaxs="i", axes=FALSE)
@@ -163,14 +166,15 @@ server <- function(input, output)
     }
   })
 
-  observeEvent(input$undo, {
+  #' @importFrom utils head
+  shiny::observeEvent(input$undo, {
                if (length(state$x$device) > 0) {
                  state$x$device <- head(state$x$device, -1)
                  state$y$device <- head(state$y$device, -1)
                }
   })
 
-  observeEvent(input$save, {
+  shiny::observeEvent(input$save, {
                file <- paste(gsub(".png$", "", state$inputFile$name), "_imager.dat", sep="")
                cat(paste("# imager version ", version, "\n", sep=""), file=file)
                cat(paste("# file:", state$inputFile$name, "\n", sep=""), file=file, append=TRUE)
@@ -193,25 +197,25 @@ server <- function(input, output)
                              i, state$x$device[i], state$y$device[i], xuser[i], yuser[i]),
                      file=file, append=TRUE)
                }
-               showNotification(paste0("File '", file, "' saved"))
+               shiny::showNotification(paste0("File '", file, "' saved"))
   })
 
-  observeEvent(input$plotHover, {
+  shiny::observeEvent(input$plotHover, {
                state$xhover <- input$plotHover$x
                state$yhover <- input$plotHover$y
   })
 
-  observeEvent(input$plotClick, {
+  shiny::observeEvent(input$plotClick, {
                if (state$step == 2) {
                  state$xaxis$device <- c(state$xaxis$device, input$plotClick$x)
-                 showModal(xAxisModal())
+                 shiny::showModal(xAxisModal())
                } else if (state$step == 3) {
-                 showModal(xAxisNameModal())
+                 shiny::showModal(xAxisNameModal())
                } else if (state$step == 4) {
                  state$yaxis$device <- c(state$yaxis$device, input$plotClick$y)
-                 showModal(yAxisModal())
+                 shiny::showModal(yAxisModal())
                } else if (state$step == 5) {
-                 showModal(yAxisNameModal())
+                 shiny::showModal(yAxisNameModal())
                } else if (state$step == 6) {
                  state$x$device <- c(state$x$device, input$plotClick$x)
                  state$y$device <- c(state$y$device, input$plotClick$y)
@@ -220,7 +224,7 @@ server <- function(input, output)
                }
   })
 
-  output$status <- renderText({
+  output$status <- shiny::renderText({
     if (is.null(state$inputFile)) {
       state$step <- 1
       return(paste0("<b>SETUP 1</b><br>Select a file, and possibly rotate it. (state$stage=", state$stage, ")"))
@@ -257,47 +261,49 @@ server <- function(input, output)
     res
   })
 
-  observeEvent(input$rotate, {
+  shiny::observeEvent(input$rotate, {
                state$rotate <- input$rotate
   })
 
-  observeEvent(input$inputFile, {
+  #' @importFrom png readPNG
+  shiny::observeEvent(input$inputFile, {
                state$inputFile <- input$inputFile
-               state$image <- readPNG(state$inputFile$datapath)
+               state$image <- png::readPNG(state$inputFile$datapath)
                state$imageExists <- TRUE
                msg("state$imageExists=", state$imageExists)
   })
 
-  observeEvent(input$xname, { state$xname <- input$xname} )
+  shiny::observeEvent(input$xname, { state$xname <- input$xname} )
 
-  observeEvent(input$yname, { state$yname <- input$yname} )
+  shiny::observeEvent(input$yname, { state$yname <- input$yname} )
 
-  observeEvent(input$xAxisButtonOk, {
+  #' @importFrom stats lm predict
+  shiny::observeEvent(input$xAxisButtonOk, {
                state$xaxis$user <- c(state$xaxis$user, as.numeric(input$xAxisValue))
                if (length(state$xaxis$user) > 1) {
                  state$xaxisModel <- lm(user ~ device, data=state$xaxis)
                  state$step <- 3
                }
-               removeModal()
+               shiny::removeModal()
   })
 
-  observeEvent(input$xAxisNameButtonOk, {
+  shiny::observeEvent(input$xAxisNameButtonOk, {
                state$xname <- input$xAxisName
-               removeModal()
+               shiny::removeModal()
   })
 
-  observeEvent(input$yAxisButtonOk, {
+  shiny::observeEvent(input$yAxisButtonOk, {
                state$yaxis$user <- c(state$yaxis$user, as.numeric(input$yAxisValue))
                if (length(state$yaxis$user) > 1) {
                  state$yaxisModel <- lm(user ~ device, data=state$yaxis)
                  state$step <- 5
                }
-               removeModal()
+               shiny::removeModal()
   })
 
-  observeEvent(input$yAxisNameButtonOk, {
+  shiny::observeEvent(input$yAxisNameButtonOk, {
                state$yname <- input$yAxisName
-               removeModal()
+               shiny::removeModal()
   })
 
   output$stage <- shiny::reactive({
@@ -308,16 +314,17 @@ server <- function(input, output)
   shiny::outputOptions(output, "stage", suspendWhenHidden=FALSE)
 
   output$readImage <- shiny::renderUI({
-    fileInput("inputFile", h5("Input file"), accept=c("image/png"))
+    shiny::fileInput("inputFile", shiny::h5("Input file"), accept=c("image/png"))
   })
 
 }
 
-#' imager
+#' R/shiny app for digitizing points in images.
 #'
 #' A shiny graphical user interface (GUI) for digitizing points in images, by
 #' means of mouse clicks. The GUI is meant to be reasonably self-explanatory.
-imager <- function()
+#' @export
+imageDigitizer <- function()
 {
   shiny::shinyApp(ui, server) #options=list(test.mode=TRUE))
 }
